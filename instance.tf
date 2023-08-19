f4d9919ab29ba6cf2f1b214b642963e503c262c8
ssharma8132 <ssharma8132@gmail.com>
2023-08-19 20:07:39 +0530

    added terraform file for ec2 instance

---
+++ instance.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+# Create a VPC
+resource "aws_instance" "web" {
+  ami = var.ami
+  instance_type = t2.micro
+}
